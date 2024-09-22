d40562137c166625fe12934cba01d8717b047a23
Muhamad Andhika Bagaskara <bagaskara04juni2002@gmail.com>
2024-09-22 19:08:56 +0700

    Fix menu.css

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,7 +3,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 
-import "./Menu.css";
+import "./menu.css";
 
 const menuLinks = [
     { path: "/", label: "Home" },
