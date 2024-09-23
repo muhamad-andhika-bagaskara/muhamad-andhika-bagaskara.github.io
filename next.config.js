143b4a117ca086cca04a80cb946d8d8ade839a92
Muhamad Andhika Bagaskara <bagaskara04juni2002@gmail.com>
2024-09-23 16:20:14 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
     // basePath: "/",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
+
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
 };
 
 module.exports = nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Logo from "/public/dk_logo.svg";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-3 justify-center items-center h-screen w-screen">
+      <Logo width={140} height={140} className="mb-3" />
       <p className="text-5xl">Dhika Portfolio</p>
       <p className="text-xl">Coming Soon!!! Stay update..</p>
     </div>
